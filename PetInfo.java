630d52c00cd7bbb89fe82fe4985c30e1a8372d1c
saachi-singh <99933900+saachi-singh@users.noreply.github.com>
2022-05-12 14:07:19 -0400

    Update PetInfo.java

--- PetInfo.java
+++ PetInfo.java
@@ -1,7 +1,33 @@
 package project;
-
+import java.util.Scanner;
+import java.lang.Math;
 public class PetInfo {
-    public static void info() {
-        System.out.println("info");
+    static Scanner scan = new Scanner(System.in);
+    public double pi;
+
+    public static void name() {
+        String petName;
+        System.out.print("Enter pet's name: ");
+        petName = scan.nextLine();
+        System.out.println("Your pet's name is " + petName);
+
+        PetInfo info = new PetInfo();
+        double radius, height;
+        System.out.print("What is the radius (inches) of the bowl you feed your pet in? ");
+        radius = scan.nextDouble();
+
+        System.out.print("What is the height (inches) of the bowl you feed your pet in? ");
+        height = scan.nextDouble();
+        System.out.println("Your pet bowl has a volume of " + info.volume(radius, height) + " cubic units.");
+
+    }
+    public PetInfo() {
+        this(Math.PI);
+    }
+    public PetInfo(double pi){
+        this.pi = pi;
+    }
+    public double volume(double radius, double height) {
+        return pi * radius * radius * height;
     }
 }
