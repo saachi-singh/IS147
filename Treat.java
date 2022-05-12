a3cd72cd96d3e7a92fb2ce7f57fb234f4f896129
saachi-singh <99933900+saachi-singh@users.noreply.github.com>
2022-05-12 14:07:57 -0400

    Update Treat.java

--- Treat.java
+++ Treat.java
@@ -1,7 +1,16 @@
 package project;
-
+import java.util.Scanner;
 public class Treat {
+    static Scanner scan = new Scanner(System.in);
+    public static String[] arrTreats = {"Milk Bones", "Peanut Butter", "Pupperoni"};
     public static void treat(){
-        System.out.println("treat");
+        int choice;
+        System.out.println("Choose a treat to give your pet:\n1. " + arrTreats[0] + "\n2. " + arrTreats[1] + "\n3. " +
+                arrTreats[2]);
+        choice = scan.nextInt();
+
+        System.out.println("You gave your pet " + arrTreats[choice-1] + ". They are happy.");
+
     }
+
 }
