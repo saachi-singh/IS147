8dc84e789355ebf66907722736c6fb24bcf611cb
saachi-singh <99933900+saachi-singh@users.noreply.github.com>
2022-05-12 14:08:21 -0400

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,9 +1,15 @@
 package project;
+// scanner class
 import java.util.Scanner;
+// date library
+import java.util.*;
 public class Welcome {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String ans;
+        // date object
+        Date date = new Date();
+        System.out.println("Access date: " + date);
         System.out.print("Welcome to Auto-Pet Feeder! Are you a pet owner?(y/n) ");
         ans = scan.nextLine();
         if (ans.equals("y") | ans.equals("yes"))
